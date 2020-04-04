dca4015f751ad0d3f061db7c55f791a9af20bd14
Sagleft <sagleft@mfc.click>
2020-04-04 10:13:46 +0400

    add data_filter function to Handler

--- controller/src/Handler.php
+++ controller/src/Handler.php
@@ -6,24 +6,28 @@
 		public $user  = null;
 		public $renderT = null;
 		public $last_error = "";
-		
+
 		private $db      = null;
 		private $enviro  = null;
-		
+
 		public function __construct() {
 			$this->enviro  = new \App\Model\Environment();
 			//$this->db      = new \App\Model\DataBase();
 			$this->logic   = new \App\Controller\Logic();
 			$this->user    = new \App\Controller\User();
 			$this->renderT = new \App\Controller\Render([]);
-			
+
 			//$this->logic->setdb($this->db);
 			//$this->user->setdb($this->db);
 			$this->logic->setUser($this->user);
 		}
-		
+
 		public function render($data = []) {
 			$this->renderT = new \App\Controller\Render($data);
 			$this->renderT->twigRender();
+		}
+
+		public function data_filter($string = '') {
+			return \App\Model\Utilities::data_filter($string);
 		}
 	}
